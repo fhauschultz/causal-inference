da9c647864c7417065aee553ca12d30d85a0d747
Frederik Plum Hauschultz <frederikplumhauschultz@Frederiks-Mac-mini.local>
2025-08-12 12:51:50 +0200

    fix tests

--- causal_inference/__init__.py
+++ causal_inference/__init__.py
@@ -1,4 +1,4 @@
-from prids.utils.modelling.causal_inference.difference_in_differences import BinaryDiD, EventStudy
-from prids.utils.modelling.causal_inference.synthetic_control import SyntheticControl
+from causal_inference.difference_in_differences import BinaryDiD, EventStudy
+from causal_inference.synthetic_control import SyntheticControl
 
 __all__ = ["EventStudy", "SyntheticControl", "BinaryDiD"]
