516a8fc00e688667854530bb1976e288ae03e632
Frederik Plum Hauschultz <frederikplumhauschultz@Frederiks-Mac-mini.local>
2025-08-12 11:46:18 +0200

    add synthetic control and diff in diff

---
+++ causal-inference/__init__.py
@@ -0,0 +1,4 @@
+from prids.utils.modelling.causal_inference.difference_in_differences import EventStudy, BinaryDiD
+from prids.utils.modelling.causal_inference.synthetic_control import SyntheticControl
+
+__all__ = ["EventStudy", "SyntheticControl", "BinaryDiD"]
