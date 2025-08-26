53f2647835b06a440ff9d5c5c1e3eed01caf775b
Frederik Plum Hauschultz <frederikplumhauschultz@Frederiks-Mac-mini.local>
2025-08-26 14:30:58 +0200

    fix

--- src/causal_inference/__init__.py
+++ src/causal_inference/__init__.py
@@ -1,4 +1,4 @@
-from causal_inference.difference_in_differences import EventStudy, Staggered
+from causal_inference.difference_in_differences import Staggered
 from causal_inference.synthetic_control import SyntheticControl
 
 __all__ = ["SyntheticControl", "Staggered"]
