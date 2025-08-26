f6ee5b69ef7f51bf4d58d1bd4e3b983cddb4f6c5
Frederik Plum Hauschultz <frederikplumhauschultz@Frederiks-Mac-mini.local>
2025-08-26 14:29:21 +0200

    linting

--- src/causal_inference/__init__.py
+++ src/causal_inference/__init__.py
@@ -1,4 +1,4 @@
-from causal_inference.difference_in_differences import BinaryDiD, EventStudy
+from causal_inference.difference_in_differences import EventStudy, Staggered
 from causal_inference.synthetic_control import SyntheticControl
 
-__all__ = ["EventStudy", "SyntheticControl", "BinaryDiD"]
+__all__ = ["SyntheticControl", "Staggered"]
