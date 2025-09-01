f95e1e9623400c297e38337e7cdd064cab332daf
Frederik Plum Hauschultz <frederikplumhauschultz@Frederiks-Mac-mini.local>
2025-09-01 12:35:44 +0200

    fix

--- src/causal_inference/synthetic_control.py
+++ src/causal_inference/synthetic_control.py
@@ -221,6 +221,8 @@ class SyntheticControl(BaseCausalInference):
         Plot a histogram of the placebo effects.
         """
         treatment_effects = self.results[self.results > self.get_experiment_date()].mean()
+        print(treatment_effects)
+
         plt.figure(figsize=(10, 6))
         plt.hist(self.placebo_effects.mean(1), color="blue", alpha=0.2)
         plt.axvline(treatment_effects["Effect"], color="blue", label="Treatment Effect", linewidth=2)
