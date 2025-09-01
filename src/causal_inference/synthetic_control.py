019b7648618afc472d61284c9b7b56988895f2c3
Frederik Plum Hauschultz <frederikplumhauschultz@Frederiks-Mac-mini.local>
2025-09-01 12:42:26 +0200

    fix plot

--- src/causal_inference/synthetic_control.py
+++ src/causal_inference/synthetic_control.py
@@ -224,7 +224,7 @@ class SyntheticControl(BaseCausalInference):
         print(treatment_effects)
 
         plt.figure(figsize=(10, 6))
-        plt.hist(self.placebo_effects.mean(1), color="blue", alpha=0.2)
+        plt.hist(self.placebo_effects.mean(0), color="blue", alpha=0.2)
         plt.axvline(treatment_effects["Effect"], color="blue", label="Treatment Effect", linewidth=2)
         plt.legend()
         plt.show()
