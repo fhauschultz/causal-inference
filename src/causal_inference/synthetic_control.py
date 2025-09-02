11e9b23b2bc9b29334cd13e8921c16ab2b20b778
Frederik Plum Hauschultz <frederikplumhauschultz@Frederiks-Mac-mini.local>
2025-09-02 21:03:42 +0200

    linting

--- src/causal_inference/synthetic_control.py
+++ src/causal_inference/synthetic_control.py
@@ -151,8 +151,8 @@ class SyntheticControl(BaseCausalInference):
     def fit(self, calculate_se=True, significance_level=None, prune_data_for_se_computation=True):
         self.model = self.model if self.model is not None else ClassicModelFitter()
         self.results = self._fit_model(self.treatment, unit_col=self.unit_col)
-        if calculate_se and self.treatment[self.unit_col].size == 1:
-            self._calculate_standard_errors(significance_level, prune_data_for_se_computation)
+        # if calculate_se and self.treatment[self.unit_col].size == 1:
+        #    self._calculate_standard_errors(significance_level, prune_data_for_se_computation)
         self.model_fitted = True
 
     def get_experiment_date(self):
